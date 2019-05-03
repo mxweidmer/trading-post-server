6da1863437b911c031b72ae7ba7aa71f0ddb8119
atiftariq786 <atiftariq786@hotmail.com>
2019-05-03 12:29:28 -0700

    server side updates

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");//new=====
+
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const flash = require('express-flash-messages');
 const session = require("express-session");
-const passport = require('passport');//new============
+const passport = require('passport');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -24,9 +25,6 @@ app.use(cors());
 /* app.get('/',function(req, res) {
   res.send("Hello from the root route");
 }); */
-
-
-
 
 // Connect to Mongoose
 mongoose.Promise = Promise;
