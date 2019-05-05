81438813b5d9e769ce6da4e42141d53b2564a4fb
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-05-05 15:57:08 -0700

    fixing cors accepted url

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use(passport.session());
 
 // enable CORS so that browsers don't block requests.
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://trading-post-server.herokuapp.com');
   res.header('Access-Control-Allow-Credentials', true),
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
